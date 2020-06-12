b9c7e3aa2215a6e12f071a27c91f5495ef8ee85c
Byron <byron19pv@gmail.com>
2020-06-12 14:27:36 -0500

    "Starting week three"

---
+++ Week_Three/app/src/main/java/com/byron/week_three/MainActivity.java
@@ -0,0 +1,14 @@
+package com.byron.week_three;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
