7a759f2b39aacb5aeb7d8d2654b25cf427e47485
Byron <byron19pv@gmail.com>
2020-06-11 20:31:40 -0500

    Actualizando interfaces

--- app/src/main/java/com/byron/week_two/MainActivity.java
+++ app/src/main/java/com/byron/week_two/MainActivity.java
@@ -2,13 +2,32 @@ package com.byron.week_two;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        button= findViewById(R.id.miButtonNext);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ConfirmData.class);
+                startActivity(intent);
+
+            }
+        });
+   }
+
+
 }
