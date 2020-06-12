f2875cad3a3a939838c1e59bea71d2ae70f6139e
Byron <byron19pv@gmail.com>
2020-06-11 20:05:01 -0500

    Subiendo avances de interfaces

---
+++ app/src/main/java/com/byron/week_two/ConfirmData.java
@@ -0,0 +1,14 @@
+package com.byron.week_two;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConfirmData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_data);
+    }
+}
