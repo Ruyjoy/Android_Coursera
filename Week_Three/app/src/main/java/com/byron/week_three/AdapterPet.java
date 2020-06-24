08225c028f31a64999844a17d030234d07c7374b
Byron <byron19pv@gmail.com>
2020-06-24 15:32:35 -0500

    "Starting week three"

--- Week_Three/app/src/main/java/com/byron/week_three/AdapterPet.java
+++ Week_Three/app/src/main/java/com/byron/week_three/AdapterPet.java
@@ -1,10 +1,72 @@
 package com.byron.week_three;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class Adapter extends RecyclerView.Adapter<> {
+import java.util.ArrayList;
+
+
+public class AdapterPet extends RecyclerView.Adapter<AdapterPet.PetViewHolder> {
+
+    ArrayList<Pets> petsList;
+    //Generamos el constructor que reciba esa lista de mascostas
+
+
+    public AdapterPet(ArrayList<Pets> petsList) {
+        this.petsList = petsList;
+    }
+
+    @NonNull
+    @Override
+    public AdapterPet.PetViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview,parent,false);
+        return new PetViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterPet.PetViewHolder holder, int position) {
+        //Coloca el contenido (texto o imagen) en el view correspondiente, ya sea un textview o ImageView etc..
+        Pets pets = petsList.get(position);
+        holder.ivPets.setImageResource(pets.getImagePet());
+        holder.ivBonesRaiting.setImageResource(pets.getImageBonesRaiting());
+        holder.ivBonesCheck.setImageResource(pets.getImageBonesCheckg());
+        holder.tvNameDog.setText(pets.getNamePet());
+        holder.tvQuantity.setText(pets.getPetQuantity());
 
 
 
+    }
 
+    @Override
+    public int getItemCount() {
+        return petsList.size();
+    }
+       //Esta clase es utilizada para evitar crear las vistas o views desde cero
+    public class PetViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView ivPets;
+        private ImageView ivBonesRaiting;
+        private ImageView ivBonesCheck;
+        private TextView tvNameDog;
+        private TextView tvQuantity;
+        //Constructor
+        public PetViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            ivPets          =   itemView.findViewById(R.id.ivPets);
+            ivBonesRaiting  =   itemView.findViewById(R.id.ivBonesRaiting);
+            ivBonesCheck    =   itemView.findViewById(R.id.ivbBonesCheck);
+            tvNameDog       =   itemView.findViewById(R.id.tvNameDog);
+            tvQuantity      =   itemView.findViewById(R.id.tvQuantity);
+
+
+
+        }
+    }
 }
--- Week_Three/app/src/main/java/com/byron/week_three/MainActivity.java
+++ Week_Three/app/src/main/java/com/byron/week_three/MainActivity.java
@@ -1,14 +1,89 @@
 package com.byron.week_three;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.appcompat.widget.Toolbar;
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity  {
+
+    ArrayList<Pets> petsList;
+    RecyclerView recyclerViewPets;
+    Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.miActionBar);
+        setSupportActionBar(toolbar);
+
+
+
+
+        petsList = new ArrayList<>();
+        //Se obtiene el identificador para el recyclerview
+        recyclerViewPets = findViewById(R.id.rvPets);
+        //Proporciona el recyclerview a un administrador de diseño predeterminado
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerViewPets.setLayoutManager(linearLayoutManager);
+
+        fillPets();
+        //Conecta el adaptador con el recyclerview
+        AdapterPet adapter = new AdapterPet(petsList);
+        recyclerViewPets.setAdapter(adapter);
+
+
     }
+
+    public boolean onCreateOptionsMenu (Menu menu){
+
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+
+
+    }
+
+    public boolean onOptionsItemSelected(MenuItem menuItem){
+        switch (menuItem.getItemId()){
+            case R.id.iconStar:
+                Intent intent = new Intent(MainActivity.this, FavoritePets.class);
+                startActivity(intent);
+            break;
+        }
+
+        return true;
+    }
+
+
+    private void fillPets (){
+
+        petsList.add(new Pets("Bambi","0",  R.drawable.dog_1,   R.drawable.hueso_raiting, R.drawable.hueso_like));
+        petsList.add(new Pets("Sam","0",    R.drawable.dog_2,   R.drawable.hueso_raiting, R.drawable.hueso_like));
+        petsList.add(new Pets("Luna","0",   R.drawable.dog_3,   R.drawable.hueso_raiting, R.drawable.hueso_like));
+        petsList.add(new Pets("Raiter","0", R.drawable.dog_4,   R.drawable.hueso_raiting, R.drawable.hueso_like));
+        petsList.add(new Pets("Lucas","0",  R.drawable.dog_5,   R.drawable.hueso_raiting, R.drawable.hueso_like));
+        petsList.add(new Pets("Sacha","0",  R.drawable.dog_6,   R.drawable.hueso_raiting, R.drawable.hueso_like));
+        petsList.add(new Pets("Lin","0",    R.drawable.dog_7,   R.drawable.hueso_raiting, R.drawable.hueso_like));
+        petsList.add(new Pets("Toby","0",    R.drawable.dog_8,   R.drawable.hueso_raiting, R.drawable.hueso_like));
+
+
+
+    }
+
+
 }
